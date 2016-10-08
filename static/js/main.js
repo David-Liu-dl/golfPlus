1311564d0dd651a4b35478bec423519aa065e6b1
Eunice <eunice@Eunices-MacBook-Pro.local>
2016-10-08 23:00:14 +1100

    change

--- static/js/main.js
+++ static/js/main.js
@@ -2,14 +2,9 @@
  * Created by yuhaoliu on 8/10/2016.
  */
 $(function () {
-    headerMenuToggle();
-    }
-)
-
-function headerMenuToggle() {
-    $('ul.nav li.dropdown').hover(function() {
+    $('ul.navbar-nav li.dropdown').hover(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-    }, function() {
+    }, function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-    });
-}
+    })
+})
