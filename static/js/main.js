8ffbfb07475d295f3863fdd75471fe872b0922e6
DavidFancy <yuhaoliu@Yuhaos-MacBook-Pro.local>
2016-10-10 19:28:44 +1100

    new

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,28 @@
  * Created by yuhaoliu on 8/10/2016.
  */
 $(function () {
+    var items = $('.slide-item').toArray();
+
+    (function recurse(counter) {
+        // get the colour
+        var item = items[counter];
+        // animate it
+        $(item).addClass('active');
+        // delete the value to save memory
+        console.log(item)
+        // add the value at the end of the array
+
+        // run it again for the next number
+        setTimeout(function() {
+            $(item).removeClass("active");
+            console.log(item);
+
+            delete items[counter];
+            items.push(item);
+            recurse(counter + 1);
+        }, 5000);
+// start it for the first number.
+    })(0);
 
 
     $('ul.navbar-nav li.dropdown').hover(function () {
