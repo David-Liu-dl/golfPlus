4bb940fd37e0ad5722b41b82b97c907cf9d02299
DavidFancy <yuhaoliu@Yuhaos-MacBook-Pro.local>
2016-10-10 21:22:09 +1100

    123

--- static/js/main.js
+++ static/js/main.js
@@ -3,28 +3,8 @@
  */
 $(function () {
     var items = $('.slide-item').toArray();
-
-    (function recurse(counter) {
-        // get the colour
-        var item = items[counter];
-        // animate it
-        $(item).addClass('active');
-        // delete the value to save memory
-        console.log(item)
-        // add the value at the end of the array
-
-        // run it again for the next number
-        setTimeout(function() {
-            $(item).removeClass("active");
-            console.log(item);
-
-            delete items[counter];
-            items.push(item);
-            recurse(counter + 1);
-        }, 5000);
-// start it for the first number.
-    })(0);
-
+    var timer;
+    var nowIndex;
 
     $('ul.navbar-nav li.dropdown').hover(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
@@ -32,5 +12,55 @@ $(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
     })
 
+    //start sliding
+    recurse(0);
 
+    function recurse(counter) {
+        // get the colour
+        var duration = 5000;
+        var item = items[counter];
+        nowIndex = counter;
+        // animate it
+        $(item).addClass('active');
+
+        if($(item).find('#bgvid').length){
+            duration = $(item).find('#bgvid').get(0).duration;
+            alert(duration);
+            duration = duration * 1000;
+        }
+        // run it again for the next number
+        timer = setTimeout(function() {
+            $(item).removeClass("active");
+            delete items[counter];
+            items.push(item);
+            recurse(counter + 1);
+        }, duration);
+        // start it for the first number.
+    }
+
+    function moveToNext() {
+        clearTimeout(timer);
+        var nowItem = items[nowIndex];
+
+        $(nowItem).removeClass("active");
+        delete nowItem;
+        items.push(nowItem);
+
+        recurse(nowIndex + 1);
+    }
+
+    function moveToPrevious() {
+        clearTimeout(timer);
+        var nowItem = items[nowIndex];
+
+        $(nowItem).removeClass("active");
+        delete nowItem;
+        items.push(nowItem);
+        var new_index = (items.length - 2) < 0 ? (items.length - 2):0;
+        recurse(new_index);
+    }
 })
+
+
+
+
